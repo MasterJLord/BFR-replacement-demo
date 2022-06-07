2e151dce338e39907d1b3d48895057fbabf6ff69
MasterJLord <jayden.cammarata@gmail.com>
2022-06-07 11:58:40 -0700

    extended the song
    synced gameplay to much more of the song so finishing the fight before the mapped out part ends is much easier.
    Also added shuffling and a discard pile since it's possible to draw your entire deck now and that matters actually.

--- demodemoalpha.py
+++ demodemoalpha.py
@@ -1,5 +1,3 @@
-
-from sqlite3 import Time
 import pygame, sys, random
 from pygame.locals import *
 pygame.init()
@@ -20,68 +18,84 @@ SongSyncing = [
     (1,100), (3, 450), (2, 800), (2, 1050), (2, 1250), (3, 1650), (3, 2000), (1, 2350), (1, 2600), (1, 2850), (3, 3200), (3, 3600), (1, 3950), (1, 4200), (1, 4450), (4, 4800), (3, 5200), (1, 5550), (1, 5800), (1, 6050), (4, 6400), (3, 6800), (1, 7150), (1, 7400), (1, 7650),
     (2, 8000), (2, 8400), (1, 8750), (1, 9000), (1, 9250), (2, 9600), (2, 10000), (3, 10350), (3, 10600), (3, 10850),
     (4, 11200),  (2, 11400),  (1, 11600),  (1, 11800),  (3, 12000),  (3, 12200),  (2, 12400),  (2, 12600),  #beats for every note was overwhelming here, just playing the cards 4 times in .4 seconds was difficult without reading them, notes could be added back in between every other one of these though
-    (4, 12800)
+    (4, 12800), (2, 13200), (3, 13550), (3, 13800), (3, 14050), (2, 14400), (2, 14800), (3, 15150), (3, 15400), (3, 15650), (2, 16000), (2, 16400), (1, 16750), (1, 17000), (1, 17250), (2, 17600), (2, 18000), (3, 18350), (3, 18600), (3, 18850),
+    (2, 19200), (2, 19450), (4, 19850), (2, 20250), (1, 20400), (1, 20550), (1, 20700), (3, 20850), (3, 21000),
+    (4, 21400), (2, 21800), (1, 21950), (1, 22100), (1, 22250), (3, 22400)
+    #4  2 1 1 1 3
 ]
 #1: energy, circle, 2: draw, triangle, 3: play, square, 4: attack, vertical line
 TimeElapsed = 0
 Hand=[]
+Discard = []
 QueuedCard = 0
 Energy = 3
-MaxEnergy = 3
+MaxEnergy = 4
 EnemyHealth = 20
 MyHealth = 15
 YPosition = random.randint(0, 1)
 XPosition = random.randint(0, 1)
 Targeted = random.randint(0, 1)
-deck = ["strike", "defend", "heal", "bonk", "defend", "strike", "strike", "defend", "heal", "bonk", "up", "down", "up", "down", "up", "down", "up", "down"]
+deck = ["strike", "defend", "heal", "bonk", "defend", "strike", "strike", "bonk",  "up", "down", "up", "down", "up", "down"]
 
 #function definition
 def strike():
     global Energy
     global EnemyHealth
     global QueuedCard
+    global Discard
     if Energy > 0:
         Energy -= 1
         EnemyHealth -= 6
+        Discard.append(QueuedCard)
         QueuedCard = 0
 def defend():
     global Energy
     global MyHealth
     global QueuedCard
+    global Discard
     if Energy > 0:
         Energy -= 1
         MyHealth += 5
+        Discard.append(QueuedCard)
         QueuedCard = 0
 def bonk():
     global Energy
     global EnemyHealth
     global QueuedCard
+    global Discard
     if Energy > 1:
         Energy -= 2
         EnemyHealth -= 8
+        Discard.append(QueuedCard)
         QueuedCard = 0
 def heal():
     global MyHealth
     global QueuedCard
+    global Discard
     MyHealth += 4
+    Discard.append(QueuedCard)
     QueuedCard = 0
 def up():
     global Energy
     global YPosition
     global QueuedCard
+    global Discard
     if Energy > 0:
         Energy -= 1
         if YPosition != 0:
             YPosition -= 1
+        Discard.append(QueuedCard)
         QueuedCard = 0
 def down():
     global Energy
     global YPosition
     global QueuedCard
+    global Discard
     if Energy > 0:
         Energy -= 1
         if YPosition != 1:
             YPosition += 1
+        Discard.append(QueuedCard)
         QueuedCard = 0
 
 #function assigning
@@ -235,7 +249,12 @@ while True:
             pygame.draw.polygon(screen, (110, 110, 110), ((82+b[1]-TimeElapsed/2, 720), (62+b[1]-TimeElapsed/2, 760), (102+b[1]-TimeElapsed/2, 760)))
             if b[1]-TimeElapsed/2 < 0:
                 del SongSyncing[0]
-                Hand.append(deck.pop(0))
+                if len(Hand) < 10:
+                    if len(deck) > 0:
+                        Hand.append(deck.pop(0))
+                    if len(deck) == 0 and len(Discard) != 0:
+                        random.shuffle(Discard)
+                        deck = Discard
         elif b[0] == 3:
             pygame.draw.rect(screen, (110, 110, 110), (62+b[1]-TimeElapsed/2, 720, 40, 40))
             if b[1]-TimeElapsed/2 < 0:
@@ -245,7 +264,7 @@ while True:
         elif b[0] == 4:
             pygame.draw.line(screen, (255, 110, 110), (82+b[1]-TimeElapsed/2, 710), (82+b[1]-TimeElapsed/2, 770), 6)
             if b[1]-TimeElapsed/2 < 0:
-                if YPosition == Targeted:
+                if YPosition == Targeted and EnemyHealth > 0:   
                     MyHealth -= 6
                     if MyHealth <= 0:
                         pygame.mixer.music.stop()
@@ -254,7 +273,8 @@ while True:
     
     #Everything else rendering
     #Hand, library, energy
-    screen.blit(FullLibrary[deck[0]], (15, 500))
+    if len(deck) > 0:
+        screen.blit(FullLibrary[deck[0]], (15, 500))
     if QueuedCard != 0:
         screen.blit(FullLibrary[QueuedCard], (82, 300))
     for h in range(len(Hand)):
@@ -264,7 +284,8 @@ while True:
     #Health bars
     pygame.draw.line(screen, (0, 140, 0), (230, 35), (230+10*EnemyHealth, 35), 8)
     pygame.draw.line(screen, (0, 140, 0), (230, 465), (230+10*MyHealth, 465), 8)
-    pygame.draw.rect(screen, (90, 0, 0), (450, 100+Targeted*150, 300, 150))
+    if EnemyHealth > 0:
+        pygame.draw.rect(screen, (90, 0, 0), (450, 100+Targeted*150, 300, 150))
     pygame.draw.rect(screen, (255, 255, 255), (450, 100, 150, 150), 3)
     pygame.draw.rect(screen, (255, 255, 255), (600, 100, 150, 150), 3)
     pygame.draw.rect(screen, (255, 255, 255), (450, 250, 150, 150), 3)
